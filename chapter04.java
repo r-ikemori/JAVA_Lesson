84791dfa5184cd1ce356372e56d133dc016f9771
r-ikemori <dragon06051996@gmail.com>
2022-11-18 11:45:50 +0900

    [add]演算子を学ぼう

---
+++ chapter04.java
@@ -0,0 +1,13 @@
+public class chapter04 {
+    public static void main(String[] args) {
+        String name = "Ryusei";
+        System.out.println(name);
+
+        name = "star";
+        System.out.println(name);
+
+        int number = 8;
+        System.out.println(number);
+
+    }
+}
